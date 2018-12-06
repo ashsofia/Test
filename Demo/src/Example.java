e6816264543bc9ea677a8fab3b8c00899738a4fb
ashitha <ashitha.v@soprasteria.com>
2018-12-06 11:34:01 +0530

    folder structure

---
+++ Demo/src/Example.java
@@ -0,0 +1,13 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		int a=5,b=5,c=0;
+		c=a+b;
+		System.out.println("The sum is "+c);
+		Example demo1=new Example();	
+	}
+
+}
+
+
